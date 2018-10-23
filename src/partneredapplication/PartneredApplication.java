274eff9d867dde1d611f5550d25608ea89328555
Edmon Snyder <43817240+MoneySnyder@users.noreply.github.com>
2018-10-23 09:20:15 -0400

    Partnered Application
    A group based project that solves a problem.

---
+++ src/partneredapplication/PartneredApplication.java
@@ -0,0 +1,22 @@
+/*
+ * Program Development Start Date : 10/23/2018
+ * Partnered Application Project
+ * Problem :
+ */
+package partneredapplication;
+
+/**
+ * Group Members
+ * @author cs.evsnyder
+ */
+
+public class PartneredApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
