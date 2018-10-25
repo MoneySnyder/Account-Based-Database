8594f53051f58adac0685bb3e6ed5cdd5962e083
Edmon Snyder <43817240+MoneySnyder@users.noreply.github.com>
2018-10-25 09:40:39 -0400

    Started work on encryption and account storage.

--- src/partneredapplication/PartneredApplication.java
+++ src/partneredapplication/PartneredApplication.java
@@ -32,6 +32,23 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+// Encryption Packages
+import java.security.Key;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.AlgorithmParameterSpec;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import javax.crypto.*;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.PBEParameterSpec;
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
 /**
  * Group Members
  * @author cs.evsnyder
@@ -43,9 +60,39 @@ public class PartneredApplication {
     // Account Information Database
     public static ArrayList<String> StoredUsernames = new ArrayList<String>();
     public static ArrayList<String> StoredPins = new ArrayList<String>();
+    public static ArrayList<String> StoredIds = new ArrayList<String>();
 
     // Main Method
     public static void main(String[] args) {
+      // Load XML File and create local storage.
+        try{
+          // Instance a document-builder in order to read file.
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setNamespaceAware(false);
+            factory.setValidating(false);
+            DocumentBuilder builder = factory.newDocumentBuilder();
+
+            File file = new File("src/partneredapplication/accountStorage.xml");
+            Document document = builder.parse(file);
+            Element accountStorage = document.getDocumentElement();
+            NodeList accounts = accountStorage.getChildNodes();
+            for(int i = 0, ii = 0, n = accounts.getLength(); i<n; i++){
+                Node child = accounts.item(i);
+                if(child.getNodeType() != Node.ELEMENT_NODE)
+                    continue;
+               Element account = (Element)child;
+               ii++;
+
+               // Add items / attributes to local storage.
+               StoredIds.add(account.getAttribute("id"));
+               StoredUsernames.add(getAccountData(findFirstNamedElement(child,"username")));
+               StoredPins.add(getAccountData(findFirstNamedElement(child,"pin")));
+
+            }
+
+        }  catch(IOException | ParserConfigurationException | SAXException e){
+            e.printStackTrace();
+        }
 
       // Creates account if one is not created.
         String computerUsername = System.getProperty("user.name");
@@ -103,4 +150,44 @@ public class PartneredApplication {
         StoredPins.add("NoPin");
       }
     }
+
+    // DES Encryption
+    public String encrypt(String str){
+      try {
+          // Encode the string into bytes using utf-8
+          byte[] utf8 = str.getBytes("UTF8");
+
+          // Encrypt
+          byte[] enc = ecipher.doFinal(utf8);
+
+          // Encode bytes to base64 to get a string
+          return new sun.misc.BASE64Encoder().encode(enc);
+      } catch (javax.crypto.BadPaddingException e) {
+      } catch (IllegalBlockSizeException e) {
+      } catch (UnsupportedEncodingException e) {
+      } catch (java.io.IOException e) {
+      }
+      return null;
+  }
+
+  // DES Decryption
+  public String decrypt(String str) {
+    try {
+        // Decode base64 to get bytes
+        byte[] dec = new sun.misc.BASE64Decoder().decodeBuffer(str);
+
+        // Decrypt
+        byte[] utf8 = dcipher.doFinal(dec);
+
+        // Decode using utf-8
+        return new String(utf8, "UTF8");
+    } catch (javax.crypto.BadPaddingException e) {
+    } catch (IllegalBlockSizeException e) {
+    } catch (UnsupportedEncodingException e) {
+    } catch (java.io.IOException e) {
+    }
+    return null;
+  }
+
+
 }
